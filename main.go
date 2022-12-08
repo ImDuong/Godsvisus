06476c0dd81671ad723a6202ff9901e334deb5b4
ImDuong <shiroyasha699969@gmail.com>
2022-12-08 15:45:24 +0700

    add an example of linked list

--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 )
 
 type (
 	Node struct {
 		Value interface{}
+		Next  *Node
 	}
 
 	NodeWraper struct {
@@ -22,7 +24,8 @@ type (
 	}
 
 	NodeWrapperList struct {
-		Nodes []NodeWraper
+		Nodes       []*NodeWraper
+		Connections []*canvas.Line
 	}
 
 	dsLayout struct {
@@ -36,23 +39,67 @@ func (ds *dsLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
 }
 
 func (ds *dsLayout) Layout(objs []fyne.CanvasObject, size fyne.Size) {
-	for _, node := range ds.component.Nodes {
-		diameter := fyne.Min(size.Width, size.Height)
-		radius := diameter / 2
-		size = fyne.NewSize(diameter, diameter)
-		centerPos := fyne.NewPos(size.Width/2, size.Height/2)
+	diameter := fyne.Min(size.Width, size.Height)
+	radius := diameter / 2
+	size = fyne.NewSize(diameter, diameter)
+
+	// hardcode hoziontal layout for a list
+	distance := fyne.NewSize(radius+10, 0)
+	for i, node := range ds.component.Nodes {
+		accumulateDistance := fyne.NewSize(
+			float32(i)*distance.Width,
+			float32(i)*distance.Height,
+		)
 
 		node.Component.Resize(size)
-		node.Component.Move(fyne.NewPos(centerPos.X-radius, centerPos.Y-radius))
 
-		node.Text.Move(fyne.NewPos(radius-node.Component.StrokeWidth, radius-node.Component.StrokeWidth-node.Text.TextSize))
+		// get the position at the center of the node
+		centerPos := fyne.NewPos(
+			radius+accumulateDistance.Width,
+			radius+accumulateDistance.Height,
+		)
+
+		// move the node to the right position: (center.X - radius, center.Y - radius)
+		node.Component.Move(fyne.NewPos(
+			centerPos.X-radius+accumulateDistance.Width,
+			centerPos.Y-radius+accumulateDistance.Height,
+		))
+
+		// move the text to the center of the node
+		node.Text.Move(fyne.NewPos(
+			centerPos.X-node.Component.StrokeWidth-float32(len(node.Text.Text))*node.Text.TextSize/4+accumulateDistance.Width,
+			centerPos.Y-node.Component.StrokeWidth-node.Text.TextSize/2+accumulateDistance.Height,
+		))
+
+		// add connecting line from the second node
+		if i > 0 {
+			// attach connnecting line's head position to the previous node
+			ds.component.Connections[i-1].Position1 = fyne.NewPos(
+				ds.component.Nodes[i-1].Component.Position2.X,
+				centerPos.Y,
+			)
+
+			// attach connnecting line's tail position to the current node
+			ds.component.Connections[i-1].Position2 = fyne.NewPos(
+				node.Component.Position1.X,
+				centerPos.Y,
+			)
+		}
 	}
-
 }
 
 func (ds *dsLayout) render() *fyne.Container {
 	canvasObjs := []fyne.CanvasObject{}
 	for i := range ds.component.Nodes {
+		// add connecting line from the second node
+		if i > 0 {
+			line := canvas.Line{
+				StrokeColor: theme.ForegroundColor(),
+				StrokeWidth: 3,
+			}
+			ds.component.Connections = append(ds.component.Connections, &line)
+			canvasObjs = append(canvasObjs, &line)
+		}
 		ds.component.Nodes[i].Component = &canvas.Circle{
 			StrokeColor: color.White,
 			StrokeWidth: 5,
@@ -81,15 +128,48 @@ func main() {
 
 	ds := &dsLayout{
 		component: NodeWrapperList{
-			Nodes: []NodeWraper{
+			Nodes: []*NodeWraper{
 				{
 					Data: Node{
-						Value: 1,
+						Value: 10,
 					},
 				},
 				{
 					Data: Node{
-						Value: 2,
+						Value: 12,
+					},
+				},
+				{
+					Data: Node{
+						Value: 69,
+					},
+				},
+				{
+					Data: Node{
+						Value: 13,
+					},
+				},
+				{
+					Data: Node{
+						Value: 5,
+					},
+				}, {
+					Data: Node{
+						Value: 753,
+					},
+				},
+				{
+					Data: Node{
+						Value: 14,
+					},
+				},
+				{
+					Data: Node{
+						Value: 28,
+					},
+				}, {
+					Data: Node{
+						Value: 11,
 					},
 				},
 			},
