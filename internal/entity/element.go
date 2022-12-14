edfdafe4ad0f181dc72655a2e2cec5c1431995d7
ImDuong <shiroyasha699969@gmail.com>
2022-12-14 18:06:11 +0700

    remove redundant NewElementWrapperList

--- internal/entity/element.go
+++ internal/entity/element.go
@@ -1,9 +1,6 @@
 package entity
 
 import (
-	"errors"
-	"reflect"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
@@ -21,27 +18,6 @@ type (
 		Nodes []*ElementWrapper
 	}
 )
-
-func NewElementWrapperList(data interface{}) (*ElementWrapperList, error) {
-	dataKind := reflect.TypeOf(data).Kind()
-	if dataKind != reflect.Slice && dataKind != reflect.Array {
-		return nil, errors.New("input data is not a slice")
-	}
-	dataSlice := reflect.ValueOf(data)
-
-	eleList := ElementWrapperList{}
-	eleList.Nodes = make([]*ElementWrapper, dataSlice.Len())
-
-	for i := 0; i < dataSlice.Len(); i++ {
-		eleList.Nodes[i] = &ElementWrapper{
-			Data: dataSlice.Index(i).Interface(),
-		}
-		if dataSlice.Index(i).CanAddr() {
-			eleList.Nodes[i].DataAddr = dataSlice.Index(i).UnsafeAddr()
-		}
-	}
-	return &eleList, nil
-}
 
 func (ew *ElementWrapper) Resize(s fyne.Size) {
 	ew.Shape.Resize(s)
